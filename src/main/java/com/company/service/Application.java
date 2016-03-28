d62985eb2e7d00a234916d0c30978168bcd51ab6
Stephan Kliche <me@stephankliche.de>
2016-03-28 12:28:24 +0200

    further updates

---
+++ src/main/java/com/company/service/Application.java
@@ -0,0 +1,23 @@
+package com.company.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+
+@SpringBootApplication
+@ImportResource("spring-config.xml")
+public class Application implements CommandLineRunner {
+	private static final Logger LOG = LoggerFactory.getLogger(Application.class);
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+		LOG.info("starting the app...");
+	}
+}
